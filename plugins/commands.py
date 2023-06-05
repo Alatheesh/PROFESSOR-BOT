2182e2fa31a4afa5af432fbcfc90bf3912fc6fad
Alatheesh <100457240+Alatheesh@users.noreply.github.com>
2023-06-05 13:57:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 BATCH_FILES = {}
 
-@Client.on_message(filters.command('start','love') & filters.incoming)
+@Client.on_message(filters.command(['start','love']) & filters.incoming)
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[           
