9f4005229174de43a1a4d9bfa08f63dcdd00fccd
Alatheesh <100457240+Alatheesh@users.noreply.github.com>
2023-06-06 15:05:24 +0530

    Update password.py

--- plugins/ExraMods/password.py
+++ plugins/ExraMods/password.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-@Client.on_message(filters.command(["genpassword", 'genpw']))
+@Client.on_message(filters.command(["passwordgen", 'genpw']))
 async def password(bot, update):
     message = await update.reply_text(text="`Processing...`")
     password = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+".lower()
@@ -14,6 +14,6 @@ async def password(bot, update):
         qw = random.choice(ST)
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
-    txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Mᴋɴ Bᴏᴛᴢ™️', url='https://t.me/mkn_bots_updates')]])
+    txt = f"<b>Limit:</b> {str(limit)} \n\n<b>Password: <code>{random_value}</code>\n\n CREDITS :<b> <a href=https://t.me/TG_LATHEESH>LATHEESH</a> </b>"
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('NTM', url='https://t.me/llathu63035')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
