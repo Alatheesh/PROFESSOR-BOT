f189930ac3be12e25d7553ec190b93df95f35910
Alatheesh <100457240+Alatheesh@users.noreply.github.com>
2023-06-07 14:27:12 +0530

    Create emoji.py

---
+++ plugins/ExraMods/emoji.py
@@ -0,0 +1,17 @@
+import emoji
+
+# Generate a list of all available emojis
+all_emojis = list(emoji.EMOJI_UNICODE.values())
+
+# Generate the code for the emoji dictionary
+code = "emoji_dict = {\n"
+for e in all_emojis:
+    # Extract the mood description from the shortcode
+    mood_desc = e.split("_face")[0][1:]
+    # Escape any special characters in the emoji string
+    emoji_str = repr(e)[1:-1].replace("'", r"\'")
+    code += f"    '{mood_desc}': '{emoji_str}',\n"
+code += "}\n"
+
+# Print the resulting code
+print(code)
