4abf3312a65a7e11a2c9c4f53b1b55d08961c632
Alatheesh <100457240+Alatheesh@users.noreply.github.com>
2023-06-07 17:48:33 +0530

    Update emoji.py

--- plugins/ExraMods/emoji.py
+++ plugins/ExraMods/emoji.py
@@ -1,30 +1,67 @@
+import re
+
 import emoji
 
-EMOJI_UNICODE = {
+def get_emoji(shortcode):
 
-    ":grinning_face_with_big_eyes:": "\U0001F603",
+    unicode_char = emoji.emojize(f':{shortcode}:')
 
-    ":beaming_face_with_smiling_eyes:": "\U0001F601",
+    name = emoji.UNICODE_EMOJI.get(unicode_char, None)
 
-    ":face_with_tears_of_joy:": "\U0001F602",
+    if name is not None:
 
-    ":rolling_on_the_floor_laughing:": "\U0001F923",
+        name = name.replace('_', ' ').capitalize()
 
-    # Add more emoji here
+    return name
+
+shortcode_to_mood = {
+
+    '😃': 'grinning_face_with_big_eyes',
+
+    '😊': 'smiling_face_with_smiling_eyes',
+
+    '😌': 'relieved_face',
+
+    '😍': 'smiling_face_with_heart_eyes',
+
+    '😇': 'smiling_face_with_halo',
+
+    '🤔': 'thinking_face',
+
+    '😢': 'crying_face',
+
+    '😠': 'angry_face',
+
+    '😴': 'sleeping_face',
+
+    '😷': 'face_with_medical_mask',
+
+    '🥰': 'smiling_face_with_three_hearts',
+
+    '🤩': 'star-struck',
+
+    '🙄': 'face_with_rolling_eyes',
+
+    '🤕': 'face_with_head-bandage',
+
+    '🧐': 'face_with_monocle',
+
+    '😖': 'confounded_face',
+
+    '🤬': 'face_with_symbols_on_mouth',
+
+    '🤗': 'hugging_face',
+
+    '🤢': 'nauseated_face',
+
+    '😱': 'face_screaming_in_fear',
 
 }
-# Generate a list of all available emojis
-all_emojis = emoji.emojize(":").split(":")
 
-# Generate the code for the emoji dictionary
-code = "emoji_dict = {\n"
-for e in all_emojis:
-    # Extract the mood description from the shortcode
-    mood_desc = e.split("_face")[0][1:]
-    # Escape any special characters in the emoji string
-    emoji_str = repr(e)[1:-1].replace("'", r"\'")
-    code += f"    '{mood_desc}': '{emoji_str}',\n"
-code += "}\n"
+emoji_dict = {}
 
-# Print the resulting code
-print(code)
+for shortcode, mood_desc in shortcode_to_mood.items():
+
+    emoji_dict[mood_desc] = get_emoji(shortcode)
+
+print(emoji_dict) 
