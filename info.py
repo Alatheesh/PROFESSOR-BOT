8d40e1c2b2a0f818fd2feacdf764e587beb1095b
LATHEESH REDDY <100457240+Alatheesh@users.noreply.github.com>
2025-04-21 12:01:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,9 +9,9 @@ from time import time
 
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
-    if value.strip().lower() in ["on", "true", "yes", "1", "enable", "y"]:
+    if str(value).strip().lower() in ["on", "true", "yes", "1", "enable", "y"]:
         return True
-    elif value.strip().lower() in ["off", "false", "no", "0", "disable", "n"]:
+    elif str(value).strip().lower() in ["off", "false", "no", "0", "disable", "n"]:
         return False
     else:
         return default
@@ -19,35 +19,58 @@ def is_enabled(value, default):
 
 # Bot information
 PORT = environ.get("PORT", "8080")
-WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
+WEBHOOK = is_enabled(environ.get("WEBHOOK", "True"), True) # Consistent boolean handling
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ['18019745'])
-API_HASH = environ['59f97cf6abf6201b862551f66eae0762']
-BOT_TOKEN = environ['6176065465:AAF3Bvumshvfjllw8m9PhpCDavFldl5zFzg']
+
+# If you want to set API_ID manually in the code:
+API_ID = 18019745  # Replace with your actual API ID
+# Otherwise, to fetch from environment with a default:
+# API_ID = int(environ.get('API_ID', '1234567'))
+
+API_HASH = environ.get('API_HASH', '59f97cf6abf6201b862551f66eae0762') # Provide a default if not always env var
+BOT_TOKEN = environ.get('BOT_TOKEN', '6176065465:AAF3Bvumshvfjllw8m9PhpCDavFldl5zFzg') # Provide a default if not always env var
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS' ,'https://telegra.ph/file/75f58d091cccefade8d5b.jpg https://telegra.ph/file/c4ddf6a9d136cb1735bb1.jpg https://telegra.ph/file/b36685221ce5ac41ad667.jpg https://telegra.ph/file/7f59377ace528148d15bd.jpg https://telegra.ph/file/e006737306ad1c5c16192.jpg https://telegra.ph/file/f8b495d98fd4d89c99150.jpg https://telegra.ph/file/320cdc500bc7e3d1c9e94.jpg https://telegra.ph/file/90ea7771a7c61e2d45d72.jpg https://telegra.ph/file/0d6adc21a51a32c3ac803.jpg https://telegra.ph/file/3fd5587f5bf4c3c107c91.jpg https://telegra.ph/file/2182f0c156d4ae25c8913.jpg')).split()
+USE_CAPTION_FILTER = is_enabled(environ.get('USE_CAPTION_FILTER', "True"), True)
+# If you want to set PICS manually:
+PICS = ["https://example.com/pic1.jpg", "https://example.com/pic2.png"]
+# Otherwise, to fetch from environment:
+# PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg ...')).split()
 BOT_START_TIME = time()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '907416893 982105601').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '907416893').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+# To manually add admins directly in the code (in addition to or instead of environment variables):
+MANUAL_ADMINS = [12345, 67890]
+ADMINS_ENV = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = list(set(MANUAL_ADMINS + ADMINS_ENV))
+
+# To manually add channels:
+MANUAL_CHANNELS = [-1001234567890, -1000987654321]
+CHANNELS_ENV = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
+CHANNELS = list(set(MANUAL_CHANNELS + CHANNELS_ENV))
+
+# To manually add authorized users:
+MANUAL_AUTH_USERS = [112233, 445566]
+auth_users_env = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+auth_users = list(set(MANUAL_AUTH_USERS + auth_users_env))
+AUTH_USERS = (auth_users + [admin for admin in ADMINS if isinstance(admin, int)]) if auth_users else [admin for admin in ADMINS if isinstance(admin, int)]
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+
+# To manually add authorized groups:
+MANUAL_AUTH_GROUPS = [-100111222333, -100444555666]
+AUTH_GROUPS_ENV = [int(ch) for ch in environ.get('AUTH_GROUP', '').split()] if environ.get('AUTH_GROUP') else []
+AUTH_GROUPS = list(set(MANUAL_AUTH_GROUPS + AUTH_GROUPS_ENV)) if MANUAL_AUTH_GROUPS or AUTH_GROUPS_ENV else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://latheesh:latheesh@cluster0.uouuowj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #maximum search result buttos count in number#
-MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
+MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10")) # You can directly assign an integer here if needed
 START_MESSAGE = environ.get('START_MESSAGE', '👋 𝙷𝙴𝙻𝙾 {user}\n\n𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 {bot},\n𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽...')
 BUTTON_LOCK_TEXT = environ.get("BUTTON_LOCK_TEXT", "⚠️ 𝙃𝙚𝙮 {query}! 𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙁𝙤𝙧 𝙔𝙤𝙪. 𝙋𝙡𝙚𝙖𝙨𝙚 𝙍𝙚𝙦𝙪𝙚𝙨𝙩 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣")
 FORCE_SUB_TEXT = environ.get('FORCE_SUB_TEXT', '𝑱𝒐𝒊𝒏 𝑶𝒖𝒓 𝑴𝒐𝒗𝒊𝒆 𝑼𝒑𝒅𝒂𝒕𝒆𝒔 𝑪𝒉𝒂𝒏𝒏𝒆𝒍 𝑻𝒐 𝑼𝒔𝒆 𝑻𝒉𝒊𝒔 𝑩𝒐𝒕!')
@@ -55,8 +78,8 @@ RemoveBG_API = environ.get("RemoveBG_API", "")
 WELCOM_PIC = environ.get("WELCOM_PIC", "")
 WELCOM_TEXT = environ.get("WELCOM_TEXT", "Hai {user}\nwelcome to {chat}")
 PMFILTER = environ.get('PMFILTER', "True")
-G_FILTER = bool(environ.get("G_FILTER", True))
-BUTTON_LOCK = environ.get("BUTTON_LOCK", "True")
+G_FILTER = is_enabled(environ.get("G_FILTER", "True"), True)
+BUTTON_LOCK = is_enabled(environ.get("BUTTON_LOCK", "True"), True)
 
 # url shortner
 SHORT_URL = environ.get("SHORT_URL")
@@ -64,10 +87,10 @@ SHORT_API = environ.get("SHORT_API")
 
 # Others
 IMDB_DELET_TIME = int(environ.get('IMDB_DELET_TIME', "300"))
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'ntmgroup')
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0)) # You can directly assign an integer here
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mkn_bots_updates')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), True)
-PM_IMDB = environ.get('PM_IMDB', "True")
+PM_IMDB = is_enabled(environ.get('PM_IMDB', "True"), True)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", "{file_name}")
@@ -76,11 +99,16 @@ IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌IMDb Da
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
-INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL)) # You can directly assign an integer here
+# To manually add file store channels:
+MANUAL_FILE_STORE_CHANNEL = [-100999888777, -100666555444]
+FILE_STORE_CHANNEL_ENV = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
+FILE_STORE_CHANNEL = list(set(MANUAL_FILE_STORE_CHANNEL + FILE_STORE_CHANNEL_ENV))
+
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
-
-
+#request force sub
+REQ_SUB = bool(environ.get("REQ_SUB", "True")) # Corrected to string for is_enabled if needed
+SESSION_STRING = environ.get("SESSION_STRING", "")
